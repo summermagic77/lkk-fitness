7ed6db7e1d066a4a5cf4de20042cb21405cc4fd9
Chiquitta <linyuting@MacBook-Pro.local>
2019-11-22 15:27:04 +0800

    route

--- src/main.js
+++ src/main.js
@@ -1,9 +1,22 @@
 import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import VueSidebarMenu from 'vue-sidebar-menu';
+import VueQrcodeReader from 'vue-qrcode-reader';
+
 import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+import 'vue-sidebar-menu/dist/vue-sidebar-menu.css';
+import './scss/custom.scss';
+
 Vue.config.productionTip = false;
+
+Vue.use(BootstrapVue);
+Vue.use(VueSidebarMenu);
+Vue.use(VueQrcodeReader);
 
 new Vue({
   router,
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Home from '../views/Home.vue';
+import Member from '../views/Member.vue';
 
 Vue.use(VueRouter);
 
@@ -17,6 +18,72 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+  },
+  {
+    path: '/member',
+    name: 'member',
+    component: Member,
+    children: [
+      {
+        path: '/:id',
+        name: 'member_info',
+        component: Member,
+      },
+      {
+        path: '/create',
+        name: 'member_create',
+        component: Member,
+      },
+      {
+        path: '/checkin',
+        name: 'member_checkin',
+        component: Member,
+      },
+    ],
+  },
+  {
+    path: '/employee',
+    name: 'employee',
+    component: Member,
+    children: [
+      {
+        path: '/:id',
+        name: 'employee_info',
+        component: Member,
+      },
+      {
+        path: '/create',
+        name: 'employee_create',
+        component: Member,
+      },
+      {
+        path: '/checkin',
+        name: 'employee_checkin',
+        component: Member,
+      },
+    ],
+  },
+  {
+    path: '/coach',
+    name: 'coach',
+    component: Member,
+    children: [
+      // {
+      //   path: '/:id',
+      //   name: 'employee_info',
+      //   component: Member,
+      // },
+      {
+        path: '/create',
+        name: 'coach_create',
+        component: Member,
+      },
+      {
+        path: '/checkin',
+        name: 'coach_checkin',
+        component: Member,
+      },
+    ],
   },
 ];
 
