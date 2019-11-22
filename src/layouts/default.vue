c9da402506f13724a33692fb5f1d29e666aa7760
Chiquitta Lin <chiquitta@brandma.co>
2019-11-23 00:58:45 +0800

    test menu

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,25 +1,27 @@
 <template>
   <div>
-    <el-container>
-        <!-- <el-header>Header</el-header> -->
-        <el-container>
-          <el-aside width="300px">
-            <side-bar />
-          </el-aside>
-          <el-main>
-              <slot />
-          </el-main>
-        </el-container>
-      </el-container>
+    <nav class="main-nav">
+      <div class="logo" />
+      <Burger></Burger>
+      </nav>
+      <Sidebar>
+        <ul class="sidebar-panel-nav">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#contact">Contact</a></li>
+        </ul>
+      </Sidebar>
   </div>
 </template>
 
 <script>
-import SideBar from '@/components/SideBar.vue';
+import Burger from '@/components/Menu/Burger.vue';
+import Sidebar from '@/components/Menu/Sidebar.vue';
 
 export default {
   components: {
-    SideBar,
+    Burger,
+    Sidebar,
   },
   data() {
     return {
@@ -29,3 +31,45 @@ export default {
   },
 };
 </script>
+<style>
+html {
+    height: 100%;
+    overflow:hidden;
+  }
+
+  body {
+    border: 0; margin: 0; padding: 0;
+    font-family: 'Lato';
+    height: 100%;
+    background: rgb(101,31,87);
+    background:
+      linear-gradient(45deg,
+      rgba(101,31,87,1) 0%,
+      rgba(225,113,87,1) 48%,
+      rgba(249,248,113,1) 100%);
+  }
+  .logo {
+    align-self: center;
+    color: #fff;
+    font-weight: bold;
+    font-family: 'Lato'
+  }
+
+  .main-nav {
+    display: flex;
+    justify-content: space-between;
+    padding: 0.5rem 0.8rem;
+  }
+
+  ul.sidebar-panel-nav {
+    list-style-type: none;
+  }
+
+  ul.sidebar-panel-nav > li > a {
+    color: #fff;
+    text-decoration: none;
+    font-size: 1.5rem;
+    display: block;
+    padding-bottom: 0.5em;
+  }
+</style>
