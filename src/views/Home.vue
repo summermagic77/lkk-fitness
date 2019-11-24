1a2c4fc6e10f9a87907b477287e8ace343ec0098
Chiquitta Lin <chiquitta@brandma.co>
2019-11-24 23:44:32 +0800

    update home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,34 @@
 <template>
   <el-container>
     <el-header class="home-header">
-      <el-row type="flex" class="row-bg" justify="center">
+      <!-- <el-row type="flex" class="row-bg" justify="center">
         <el-col :span="6"><div class="grid-content bg-purple-light">Header</div></el-col>
-      </el-row>
+      </el-row> -->
     </el-header>
     <el-main>
       <el-row :gutter="20" type="flex" class="row-bg" justify="center">
         <el-col :xs="12" :sm="12" :md="6" :lg="6" :xl="6" class="text-center">
-            <el-card :body-style="{ padding: '0px' }" shadow="hover">
-              <el-link href="/member" class="w-100" :underline="false" style="padding: 20px 0;">
-                <!-- <h1><i class="el-icon-plus" /></h1> -->
-                <account-check class="icon-3x" />
-                <h3>
-                  會員進場
-                </h3>
-              </el-link>
-            </el-card>
+          <el-card :body-style="{ padding: '0px' }" shadow="hover">
+            <el-link href="/member" class="w-100 py-2" :underline="false">
+              <account-check class="icon-4x" />
+              <h1 class="font-weight-light">
+                會員進場
+              </h1>
+            </el-link>
+          </el-card>
         </el-col>
         <el-col :xs="12" :sm="12" :md="6" :lg="6" :xl="6" class="text-center">
-          <el-card shadow="hover">
-            <!-- <h1><i class="el-icon-plus" /></h1> -->
-            <h1>
-              <account-plus class="icon-3x" />
-            </h1>
-            建立新會員
+          <el-card :body-style="{ padding: '0px' }" shadow="hover">
+            <el-link href="/member" class="w-100 py-2" :underline="false">
+              <account-plus class="icon-4x" />
+              <h1 class="font-weight-light">
+                建立新會員
+              </h1>
+            </el-link>
           </el-card>
         </el-col>
       </el-row>
-      <el-row :gutter="20" type="flex" class="row-bg" justify="center">
+      <!-- <el-row :gutter="20" type="flex" class="row-bg" justify="center">
         <el-col :span="6">
           <el-card shadow="hover" class="text-center">
             <h1>
@@ -40,13 +40,12 @@
         <el-col :span="6">
           <el-card shadow="hover" class="text-center">
             <h1>
-              <!-- <font-awesome-icon icon="id-card-alt" class="fa-2x" /> -->
               <account-multiple-check class="icon-3x" />
             </h1>
             教練場租
           </el-card>
         </el-col>
-      </el-row>
+      </el-row> -->
     </el-main>
   </el-container>
 </template>
@@ -56,16 +55,16 @@
 // import HelloWorld from '@/components/HelloWorld.vue';
 import AccountCheck from 'vue-material-design-icons/AccountCheck.vue';
 import AccountPlus from 'vue-material-design-icons/AccountPlus.vue';
-import AccountBadgeOutline from 'vue-material-design-icons/AccountBadgeOutline.vue';
-import AccountMultipleCheck from 'vue-material-design-icons/AccountMultipleCheck.vue';
+// import AccountBadgeOutline from 'vue-material-design-icons/AccountBadgeOutline.vue';
+// import AccountMultipleCheck from 'vue-material-design-icons/AccountMultipleCheck.vue';
 
 export default {
   name: 'home',
   components: {
     AccountCheck,
     AccountPlus,
-    AccountBadgeOutline,
-    AccountMultipleCheck,
+    // AccountBadgeOutline,
+    // AccountMultipleCheck,
   },
 };
 </script>
@@ -74,19 +73,25 @@ export default {
 /* .home-header {
   background-color: #29526b;
 } */
-.material-design-icon.icon-3x {
-  height: 3em;
-  width: 3em;
+.material-design-icon.icon-4x {
+  height: 4em;
+  width: 4em;
 }
-.material-design-icon.icon-3x > .material-design-icon__svg {
-  height: 3em;
-  width: 3em;
+.material-design-icon.icon-4x > .material-design-icon__svg {
+  height: 4em;
+  width: 4em;
 }
 .text-center {
   text-align: center;
 }
 .w-100 {
   width: 100%;
+}
+.py-2 {
+  padding: 2em 0;
+}
+.font-weight-light {
+  font-weight: 300;
 }
 .el-row {
   margin-bottom: 20px;
