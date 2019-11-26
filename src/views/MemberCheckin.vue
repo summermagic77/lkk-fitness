a2d3a5d0a07aac06e284b2e63d68973f054c953a
Chiquitta <linyuting@MacBook-Pro.local>
2019-11-26 14:54:19 +0800

    checkin

--- src/views/MemberCheckin.vue
+++ src/views/MemberCheckin.vue
@@ -1,13 +1,63 @@
 <template>
-  <div>
-    {{ $route.name }}
-  </div>
+  <el-row
+    :gutter="20"
+    type="flex"
+    class="row-bg"
+    justify="center"
+    align="middle"
+  >
+    <el-col :xs="12" :sm="12" :md="6" :lg="6" :xl="6" class="text-center">
+      <el-radio-group v-model="type">
+        <el-radio
+          v-for="(item, idx) in searchType"
+          :key="idx"
+          :label="item.value"
+        >
+          {{ item.label }}
+        </el-radio>
+      </el-radio-group>
+      <el-input
+        v-model="input"
+        :type="inputType[type].type"
+        :placeholder="inputType[type].placeholder"
+        :pattern="inputType[type].pattern"
+        class="mt-2"
+        suffix-icon="el-icon-search"
+      />
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 export default {
   data() {
     return {
+      input: '',
+      type: 'phone',
+      inputType: {
+        phone: {
+          type: 'text',
+          placeholder: '請輸入會員手機號碼進行搜尋',
+        },
+        name: {
+          type: 'text',
+          placeholder: '請輸入會員姓名進行搜尋',
+        },
+      },
+      searchType: [
+        {
+          label: '手機',
+          value: 'phone',
+        },
+        {
+          label: '姓名',
+          value: 'name',
+        },
+        {
+          label: 'QR Code',
+          value: 'qrcode',
+        },
+      ],
     };
   },
   methods: {
