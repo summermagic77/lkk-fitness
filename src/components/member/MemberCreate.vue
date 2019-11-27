b790487778d92a096b25fef98f9fd915f2ac3bd3
Chiquitta <linyuting@MacBook-Pro.local>
2019-11-27 13:59:57 +0800

    steps

--- src/components/member/MemberCreate.vue
+++ src/components/member/MemberCreate.vue
@@ -6,45 +6,82 @@
     align="middle"
   >
     <el-col :sm="12" :md="12" :lg="8" :xl="8">
-      <h1 class="mb-0">
+      <el-steps :active="activeStep" align-center class="mb-2">
+        <el-step title="基本資料" description="填寫會員資料" />
+        <el-step title="入會資訊" description="填寫入會資訊" />
+      </el-steps>
+      <!-- <h1 class="mb-0">
         建立新會員
       </h1>
       <p class="text-black-50 mb-1">
         填寫完整會員資料
-      </p>
+      </p> -->
       <el-form
         :model="ruleForm"
         :rules="rules"
         status-icon
         ref="ruleForm"
+        :label-position="activeStep === 2 ? 'top' : 'left'"
       >
-        <el-form-item prop="name">
-          <el-input v-model="ruleForm.name" placeholder="姓名" />
-        </el-form-item>
-        <el-form-item prop="sex">
-          <el-radio-group v-model="ruleForm.sex">
-            <el-radio border label="男" />
-            <el-radio border label="女" />
-          </el-radio-group>
-        </el-form-item>
-        <el-form-item prop="phone">
-          <el-input v-model="ruleForm.phone" placeholder="手機號碼" />
-        </el-form-item>
-        <el-form-item prop="email">
-          <el-input v-model="ruleForm.email" placeholder="Email" />
-        </el-form-item>
-        <el-form-item label="點數" prop="point">
-          <el-input-number v-model="ruleForm.point" :step="100" />
-        </el-form-item>
-        <el-form-item label="加入日期" prop="join_date">
-          <el-date-picker
-            v-model="ruleForm.join_date"
-            type="date"
-            placeholder="選擇加入日期"
-          />
-        </el-form-item>
+        <div v-if="activeStep === 1">
+          <el-form-item prop="name">
+            <el-input v-model="ruleForm.name" placeholder="姓名" />
+          </el-form-item>
+          <el-form-item prop="sex">
+            <el-radio-group v-model="ruleForm.sex">
+              <el-radio border label="男" />
+              <el-radio border label="女" />
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item prop="phone">
+            <el-input v-model="ruleForm.phone" placeholder="手機號碼" />
+          </el-form-item>
+          <el-form-item prop="email">
+            <el-input v-model="ruleForm.email" placeholder="Email" />
+          </el-form-item>
+        </div>
+        <div v-else>
+          <el-form-item label="會員等級" prop="level">
+            <el-radio-group v-model="ruleForm.level">
+              <el-radio border label="計次" />
+              <el-radio border label="月費" />
+            </el-radio-group>
+          </el-form-item>
+          <el-form-item label="會員點數" prop="point">
+            <el-input-number
+              v-model="ruleForm.point"
+              :step="100"
+              placeholder="會員點數"
+              class="w-100"
+            />
+          </el-form-item>
+          <el-form-item label="加入日期" prop="join_date">
+            <el-date-picker
+              v-model="ruleForm.join_date"
+              type="date"
+              placeholder="選擇加入日期"
+              class="w-100"
+            />
+          </el-form-item>
+        </div>
         <el-form-item>
-          <el-button type="primary" class="w-100" @click="submitForm('ruleForm')">建立會員資料</el-button>
+          <!-- <div v-if="activeStep === 1"> -->
+          <el-button
+            v-if="activeStep === 1"
+            type="primary"
+            plain
+            class="w-100"
+            @click="activeStep++"
+          >
+            下一步
+          </el-button>
+          <!-- </div> -->
+          <div v-else>
+            <el-button @click="activeStep--">上一步</el-button>
+            <el-button type="primary" @click="submitForm('ruleForm')" class="float-right">
+              建立會員資料
+            </el-button>
+          </div>
           <!-- <el-button type="success" icon="el-icon-check" circle /> -->
         </el-form-item>
       </el-form>
@@ -56,12 +93,14 @@
 export default {
   data() {
     return {
+      activeStep: 2,
       ruleForm: {
-        name: '',
-        sex: '男',
-        email: '',
-        point: 100,
-        join_date: new Date(),
+        name: 'Chiquitta',
+        sex: '女',
+        phone: '0987654321',
+        email: 'chiquitta.com@gmail.com',
+        // point: 0,
+        join_date: '',
       },
       rules: {
         name: [
